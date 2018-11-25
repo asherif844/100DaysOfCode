c7ac2224762231240577bc4a36e6c50f7c309a48
asherif844 <ahmed.sherif@att.net>
2018-11-25 15:28:59 -0500

    Day 8 updated

--- FlaskMLPart2/app.py
+++ FlaskMLPart2/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, request
-import pandas as import pd
+import pandas as pd
 import pickle 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
 
