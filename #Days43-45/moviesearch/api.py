782b657e6482293c8d4dcae5ab401d18ba4466e0
asherif844 <ahmed.sherif@att.net>
2019-01-21 13:55:15 -0500

    #100DaysofCode

--- #Days43-45/moviesearch/api.py
+++ #Days43-45/moviesearch/api.py
@@ -2,4 +2,8 @@ import requests
 
 
 def find_movie_by_title(keyword):
-    pass
+    url = f'http://movie_service.talkpython.fm/api/search/{keyword}'
+    resp = requests.get(url)
+    resp.raise_for_status()
+    results = resp.json()
+    return results.get('hits')
--- #Days43-45/moviesearch/program.py
+++ #Days43-45/moviesearch/program.py
@@ -1,7 +1,11 @@
+import api
 
 
 def main():
-    pass
+    results = api.find_movie_by_title(keyword='men')
+    for r in results:
+        print(f"There are {len(r)} movies found.")
+        print(f"Title: {r.get('title')}")
 
 
 if __name__ == '__main__':
