51cba8f786cd07634487e93bdffbd65af795ed43
Ahmed <ahsherif@microsoft.com>
2019-03-25 19:51:32 -0400

    update for file

--- #Days46-48/scraper.2.py
+++ #Days46-48/scraper.2.py
@@ -54,6 +54,27 @@ def printComicBooks(page_count):
 
 listOfComics = list(printComicBooks(max_page_count))
 
+name_of_comic = []
+number_of_comic = []
+year_of_comic = []
+publisher = []
 for i in listOfComics:
-    # print(i.split(':'))
-    print(i.split('(', 1)[1].split(')')[0])
+    one = i.split('(')[0]
+    name_of_comic.append(one)
+    two = i.split(':')[1]
+    number_of_comic.append(two)
+    three = i.split('(', 1)[1].split(')')[0]
+    four = three.split(' ')[0]
+    year_of_comic.append(four)
+    five = three.split(' ')[1:]
+    five = ' '.join(five)
+    publisher.append(five)
+ 
+
+df = pd.DataFrame({'name':name_of_comic,
+              'number': number_of_comic,
+              'year':year_of_comic,
+              'publisher': publisher})
+df.head()
+
+df.to_csv('#Days46-48/MyComicBookShop.csv')
